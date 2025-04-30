12688d407c33549b5db31af05e720d5f4ca8524a
Catherine <z5359881@student.unsw.edu.au>
2025-04-30 14:17:07 +1000

    creation of cards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <PersonalProjects />
-      <button> hello </button>
     </>
   )
 }
--- frontend/src/pages/PersonalProjects.tsx
+++ frontend/src/pages/PersonalProjects.tsx
@@ -1,9 +1,22 @@
 // import React from "react";
 
+import Card from "../components/Card"
+
 export default function PersonalProjects() {
   return (
     <>
-      Hellow
+      <title>cath's space - personal projects</title>
+      <section className="lexend text-5xl">
+        Personal Projects
+      </section>
+
+      <section className="mt-16 grid grid-cols-3 gap-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </section>
     </>
   );
 }
